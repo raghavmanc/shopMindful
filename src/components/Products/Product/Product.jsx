82f5e7eebfaa594605f18f76741f2a8ed730532d
raghav bhardwaj <raghavmanc@gmail.com.com>
2022-11-02 13:51:12 -0400

    Implemented Navbar and added styles.js plus improved folder structure

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -7,7 +7,7 @@ const Product = ({product}) => {
     const classes = useStyles();
   return (
     <Card className={classes.root}>
-        <CardMedia className={classes.media} image='' title={product.name}/>
+        <CardMedia className={classes.media} image={product.img} title={product.name}/>
         <CardContent>
             <div className={classes.cardContent}>
                 <Typography variant='h5' gutterBottom>
@@ -17,14 +17,13 @@ const Product = ({product}) => {
                     {product.price}
                 </Typography>
             </div>
-            <Typography variant='h2' color = 'textSecondary'>{product.desc} </Typography>
+            <Typography variant='body2' color = 'textSecondary'>{product.desc} </Typography>
         </CardContent>
         <CardActions disableSpacing className={classes.CardActions}>
             <IconButton aria-label="Add to Cart"> 
                 <AddShoppingCart/>
             </IconButton>
         </CardActions>
-
     </Card>
   )
 }
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,22 +1,28 @@
 import React from "react";
 import {Grid} from '@material-ui/core'
 import Product from "./Product/Product";
+import useStyles from './styles'
+
+
 const products = [
-    {id:1, name: 'shoes', desc:'running shoes', price: '$5'},
-    {id:2, name: 'watch', desc:'running watch', price: '$5'},
-    {id:3, name: 'Dell Laptop', desc:'DELL G7 Gaming', price: '$5'}
+    {id:1, name: 'shoes', desc:'running shoes', price: '$5',img: 'https://images.pexels.com/photos/786003/pexels-photo-786003.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'},
+    {id:2, name: 'watch', desc:'running watch', price: '$5',img: 'https://images.pexels.com/photos/4041181/pexels-photo-4041181.jpeg?auto=compress&cs=tinysrgb&w=600'},
+    {id:3, name: 'Dell Laptop', desc:'DELL G7 Gaming', price: '$5',img: 'https://images.pexels.com/photos/3987066/pexels-photo-3987066.jpeg?auto=compress&cs=tinysrgb&w=600'}
 ];
 
 const Products = () => {
 
+    const classes = useStyles();
+
     return (
-        <main>
+        <main className={classes.content}>
+            <div className={classes.toolbar}/>
         <Grid container justify = 'center' spacing = {4}>
             {products.map((product)=>(
                 <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
                     <Product product={product}/>
                 </Grid>
-            ))}
+            ))} 
         </Grid>
     </main>
 
